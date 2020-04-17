bfe9cff905172bb5fa532b9b37de43792b727d28
3TUSK <urey.s.knowledge@gmail.com>
2020-04-16 21:01:55 -0700

    Let's try doing something for GH-1

--- src/main/java/info/tritusk/PlacePreview.java
+++ src/main/java/info/tritusk/PlacePreview.java
@@ -25,7 +25,7 @@
  * For more information, please refer to <http://unlicense.org/>
  */
 
- // SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: Unlicense
 
 package info.tritusk;
 
@@ -47,7 +47,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
 import net.minecraft.entity.player.PlayerEntity;
@@ -55,11 +58,13 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
@@ -151,7 +156,29 @@ public final class PlacePreview {
                         transforms.push();
                         BlockPos target = context.getPos();
                         transforms.translate(target.getX(), target.getY(), target.getZ());
-                        mc.getBlockRendererDispatcher().renderBlock(placeResult, transforms, renderBuffer, 0xF000F0, OverlayTexture.NO_OVERLAY, EmptyModelData.INSTANCE);
+                        World world = context.getWorld();
+                        switch (placeResult.getRenderType()) {
+                            case MODEL:
+                                mc.getBlockRendererDispatcher().renderModel(placeResult, target, world, transforms, renderBuffer.getBuffer(RenderTypeLookup.getRenderType(placeResult)), EmptyModelData.INSTANCE);
+                            case ENTITYBLOCK_ANIMATED:
+                                /* 
+                                 * Yes, we use a fake tile entity to workaround this.
+                                 * All exceptions are discared. It is ugly, yes, but
+                                 * it partially solve the problem.
+                                 */
+                                if (placeResult.hasTileEntity()) {
+                                    TileEntity tile = placeResult.createTileEntity(world);
+                                    tile.setWorldAndPos(world, target);
+                                    TileEntityRenderer<? super TileEntity> renderer = TileEntityRendererDispatcher.instance.getRenderer(tile);
+                                    if (renderer != null) {
+                                        try {
+                                            renderer.render(tile, 0F, transforms, renderBuffer, world.getLight(target), OverlayTexture.NO_OVERLAY);
+                                        } catch (Exception ignored) {}
+                                    }
+                                }
+                            default:
+                                break;
+                        }
                         transforms.pop();
                         renderBuffer.finish();
                     }
